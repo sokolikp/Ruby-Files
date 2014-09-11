33dbf3d571148c3fea7dfc7040310bff8de23c5a
sokolikp <paul.sokolik@gmail.com>
2014-09-11 14:48:25 -0500

    Create TableOfContents.rb

---
+++ TableOfContents.rb
@@ -0,0 +1,10 @@
+line_width = 70
+
+puts ('Table of Contents').center(line_width)
+puts ('Chapter 1: Getting Started').ljust(line_width/2) + 
+     ('Page 1').rjust(line_width/2)
+puts ('Chapter 2: Numbers').ljust(line_width/2) + 
+     ('Page 7').rjust(line_width/2)
+puts ('Chapter 3: Letters').ljust(line_width/2) + 
+     ('Page 27').rjust(line_width/2)
+#puts 'this is a test'.rjust(line_width)
